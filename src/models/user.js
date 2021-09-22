525b341bf13a5975e2f9655ebb4591df282aa584
snootiecute <dangtiendat288@gmail.com>
2021-09-22 21:55:45 +0700

    includes virtual property (tasks) to userSchema.toObject()'s output

--- src/models/user.js
+++ src/models/user.js
@@ -57,7 +57,7 @@ const userSchema = new mongoose.Schema(
     ],
     avatar: { type: Buffer },
   },
-  { timestamps: true }
+  { timestamps: true, toObject: { virtuals: true } }
 );
 
 userSchema.virtual("tasks", {
